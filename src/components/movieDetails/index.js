8f6053d3611dab89ff0653f91ad57acba7a1e9c7
varghese00 <varghese00@gmail.com>
2022-07-11 14:53:20 +0100

    Show movie review excerpts; Refactor Details page.

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
@@ -6,6 +6,11 @@ import MonetizationIcon from "@material-ui/icons/MonetizationOn";
 import StarRate from "@material-ui/icons/StarRate";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+// New
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Fab from "@material-ui/core/Fab";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from '../movieReviews'
 
 const useStyles = makeStyles((theme) => ({
   chipRoot: {
@@ -30,16 +35,19 @@ const useStyles = makeStyles((theme) => ({
   chipLabel: {
     margin: theme.spacing(0.5),
   },
+  fab: {  //New
+    position: "fixed",
+    top: theme.spacing(15),
+    right: theme.spacing(2),
+  },
 }));
 
-const MovieDetails = ( props) => {
+const MovieDetails = ( {movie}) => {
   const classes = useStyles();
-  const movie = props.movie
+  const [drawerOpen, setDrawerOpen] = useState(false); // New
 
   return (
-
     <>
-
       <Typography variant="h5" component="h3">
         Overview
       </Typography>
@@ -47,14 +55,9 @@ const MovieDetails = ( props) => {
       <Typography variant="h6" component="p">
         {movie.overview}
       </Typography>
-
-
       <div className={classes.chipRoot}>
-
-{/* Paper for genres */}
       <Paper component="ul" className={classes.chipSet}>
         <li>
-            {/* chip is the icon used for genres ,fantasy ,Released etc.. */}
           <Chip label="Genres" className={classes.chipLabel} color="primary" />
         </li>
         {movie.genres.map((g) => (
@@ -63,8 +66,6 @@ const MovieDetails = ( props) => {
           </li>
         ))}
       </Paper>
-
-{/* Paper for release and other stuffs */}
       <Paper component="ul" className={classes.chipSet}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
@@ -77,21 +78,21 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
-
-{/* Paper for production countries */}
-      <Paper component="ul" className={classes.chipSet}>
-        <li>
-          <Chip label="Production Countries" className={classes.chipLabel} color="primary" />
-        </li>
-        {movie.production_countries.map((p) => (
-          <li key={p.name}>
-            <Chip label={p.name} className={classes.chip} />
-          </li>
-        ))}
-      </Paper>
-
       </div>
-      </>
+      {/* New */}
+      <Fab    
+        color="secondary"
+        variant="extended"
+        onClick={() =>setDrawerOpen(true)}
+        className={classes.fab}
+      >
+        <NavigationIcon />
+        Reviews
+      </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
+    </>
   );
 };
 export default  MovieDetails ;
