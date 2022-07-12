e013568546f4e0197e5031d1f6142a8a2f07db3d
varghese00 <varghese00@gmail.com>
2022-07-12 09:58:04 +0100

    Refactor - useMovie custom hook.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,22 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import useMovie from "../hooks/useMovie";
 
 const MovieDetailsPage = (props) => {
   const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const [movie] = useMovie(id);  // New
 
   return (
     <>
-      {movie ? ( // ternary operator to performs the condition test
+      {movie ? (
         <>
           <PageTemplate movie={movie}>
             <MovieDetails movie={movie} />
