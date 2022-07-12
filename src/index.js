fdcb089164c7cbf43c3f91201e6b1bde5caaba4f
varghese00 <varghese00@gmail.com>
2022-07-12 09:33:10 +0100

    Full review page.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -23,6 +24,8 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+
       </Routes>
       
     </BrowserRouter>
